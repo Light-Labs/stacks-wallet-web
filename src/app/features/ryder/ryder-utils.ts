5d30dcba92b0ac3ab82342d2c60f70ac19bde96b
friedger <mail@friedger.de>
2022-08-30 22:33:29 +0200

    use COMMAND_EXPORT_DERIVED_PUBLIC_KEY to export identiy keys

--- src/app/features/ryder/ryder-utils.ts
+++ src/app/features/ryder/ryder-utils.ts
@@ -27,7 +27,7 @@ const port = 'ws:/localhost:8888';
 const stxDerivationWithoutAccount = `m/44'/5757'/0'/0/`;
 const identityDerivationWithoutAccount = `m/888'/0'/`;
 export class StacksApp {
-  constructor(_: any) {}
+  constructor(_: any) { }
   signGetChunks(path: string, message: Buffer): Promise<Buffer[]> {
     return Promise.reject('signGetChunks not implemented');
   }
@@ -267,9 +267,21 @@ class RyderApp {
           reject('Ryder serial was destroyed');
           return;
         }
-        const response = await this.ryder_serial.send(
-          [RyderSerial.COMMAND_EXPORT_DERIVED_PUBLIC_KEY].concat(pathToBytes(path))
+        let response = await this.ryder_serial.send(
+          [RyderSerial.COMMAND_EXPORT_DERIVED_PUBLIC_KEY]
         );
+
+        // eslint-disable-next-line no-console
+        console.log('typeof response', typeof response);
+        let sendInput =
+          typeof response === 'number'
+            ? response.toString()
+            : Buffer.from(response, 'binary').toString('hex');
+        console.log({ sendInput });
+        response = await this.ryder_serial.send(
+          pathToBytes(path)
+        );
+
 
         // eslint-disable-next-line no-console
         console.log('typeof response', typeof response);
@@ -305,55 +317,10 @@ class RyderApp {
     index: number,
     callback: (res: Response<string>) => void
   ): Promise<void> {
-    this.ryder_serial = new RyderSerial(payload.port, payload.options);
-    new Promise<string>((resolve, reject) => {
-      if (!this.ryder_serial) {
-        reject('Ryder Serial does not exist for some reason');
-        return;
-      }
-
-      this.ryder_serial.on('failed', (error: Error) => {
-        reject(
-          `Could not connect to the Ryder on the specified port. Wrong port or it is currently in use: ${error}`
-        );
-        return;
-      });
-      this.ryder_serial.on('open', async () => {
-        if (!this.ryder_serial) {
-          reject('Ryder serial was destroyed');
-          return;
-        }
-        const response = await this.ryder_serial.send([
-          RyderSerial.COMMAND_EXPORT_PUBLIC_IDENTITY,
-          index,
-        ]);
-        // eslint-disable-next-line no-console
-        console.log('typeof response', typeof response);
-        const publicKey =
-          typeof response === 'number'
-            ? response.toString()
-            : Buffer.from(response, 'binary').toString('hex');
-        // eslint-disable-next-line no-console
-        console.log(
-          publicKey
-          //publicKeyToAddress(AddressVersion.MainnetSingleSig, createStacksPublicKey(publicKey))
-        );
-        resolve(publicKey);
-        return;
-      });
-      this.ryder_serial.on('wait_user_confirm', () => {
-        resolve('Confirm or cancel on Ryder device.');
-        return;
-      });
-    })
-      .then((res: string) => callback({ data: res }))
-      .catch(error =>
-        callback({
-          source: error,
-          error: error,
-        })
-      )
-      .finally(() => this.ryder_serial?.close());
+    this.export_derived_public_key(payload,
+      `m/888'/0'/${index}'`,
+      callback
+    )
   }
 
   async serial_export_app_private_key(
